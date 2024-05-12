dfa1c5070a81cf7efefe99ca79dea7c4813976a2
lzt <1162300237@qq.com>
2024-05-12 23:02:39 +0800

    feat: 添加icon组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
-import dayjs, { Dayjs } from 'dayjs'
-import Calendar, { CalendarProps } from './components/Calendar'
+import IconAdd from './components/Icon/icons/IconAdd'
+import IconEmail from './components/Icon/icons/IconEmail'
+import IconfontMsg from './components/Icon/icons/IconfontMsg'
 
 function App() {
-  const dateRender: CalendarProps['dateRender'] = date => {
-    return (
-      <div
-        className="date"
-        style={{
-          height: '300px',
-          // backgroundColor: 'lightblue',
-        }}
-      >
-        {date.format('YYYY-MM-DD')}
-      </div>
-    )
-  }
-
-  const dateInnerContent: CalendarProps['dateInnerContent'] = date => {
-    return (
-      <div>
-        <p
-          style={{
-            // background: 'yellowgreen',
-            height: '30px',
-          }}
-        >
-          {date.date()}
-        </p>
-      </div>
-    )
-  }
-
-  const handleChange = (date: Dayjs) => {
-    console.log(date.format('YYYY-MM-DD'))
-  }
-
   return (
     <div className="App">
-      <Calendar
-        value={dayjs('2024-05-31')}
-        className={'aaa'}
-        // style={{
-        //   background: 'grey',
-        // }}
-        locale="en-US"
-        // dateRender={dateRender}
-        // dateInnerContent={dateInnerContent}
-        onChange={handleChange}
-      ></Calendar>
+      <IconAdd size="40px" spin></IconAdd>
+      <IconEmail style={{ color: 'blue', fontSize: '50px' }}></IconEmail>
+      <IconfontMsg type="icon-xiaoxi-zhihui"></IconfontMsg>
     </div>
   )
 }
---
+++ src/components/Icon/icons/IconfontMsg.tsx
@@ -0,0 +1,7 @@
+import { createFromIconfont } from '../createFromIconfont'
+
+const IconfontMsg = createFromIconfont(
+  '//at.alicdn.com/t/c/font_4543469_5mgglglodm8.js'
+)
+
+export default IconfontMsg
